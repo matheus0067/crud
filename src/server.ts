8ef0b0c82a292b9848a08f42af0e47a9256944a1
matheus0067 <84738179+matheus0067@users.noreply.github.com>
2022-08-28 22:37:29 -0300

    adding routes in the application

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import "reflect-metadata";
-
 import express from "express";
+
+import "./database";
+import { routes } from './routes';
 
 const app = express();
 
-app.listen(3000, () => console.log("Serer is running"));
+app.use(routes);
+
+app.listen(3000, () => console.log("Server is running"));
